79864ec1b748372a360398d546428ce2a1887d8a
natan1988 <natan88@mail.ru>
2020-09-14 01:57:10 +0000

    test

--- deployments/terraform/set-env.sh
+++ deployments/terraform/set-env.sh
@@ -1,93 +1,27 @@
-#!/bin/bash -e 
+#!/usr/bin/env bash
 
-DATAFILE="$PWD/$1"
-#
-# FuchiCorp common script to set up Google terraform environment variables
-# these all variables should be created on your config file before you run script.
-# <ENVIRONMENT> <BUCKET> <DEPLOYMENT> <PROJECT> <CREDENTIALS>
-
+# condition in [] will check variable GIT_TOKEN is empty or no, if "empty" return 1 
 if [ -z "$GIT_TOKEN" ]
 then
   echo """
   Script needs github token to get private repos.
-  Github <GIT_TOKEN> not found."""
-  return 1 
-fi
+  Github <GIT_TOKEN> not found.
+  
+    run command  < export GIT_TOKEN='your_token'>
+  
+   """
+  #return 1
 
-wget --quiet "--header=Authorization: token $GIT_TOKEN" -O "$PWD/common_configuration.tfvars"\
-  "https://raw.githubusercontent.com/fuchicorp/main-fuchicorp/master/project-configuration/google_account_information.tfvars"
+# if we have variable GIT_TOKEN, condition in [] will check is it valid token or no.
+elif  [ "$(curl -s -H "Authorization: token $GIT_TOKEN" 'https://api.github.com' -I | awk '{print $2}' | head -1)" != "200"  ]
+then 
+   echo "GIT_TOKEN is not valid"
 
 
-PROJECT=$(sed -nr 's/^google_project_id\s*=\s*"([^"]*)".*$/\1/p'            "$DATAFILE")
-BUCKET=$(sed -nr 's/^google_bucket_name\s*=\s*"([^"]*)".*$/\1/p'            "$DATAFILE")
-ENVIRONMENT=$(sed -nr 's/^deployment_environment\s*=\s*"([^"]*)".*$/\1/p'   "$DATAFILE")
-DEPLOYMENT=$(sed -nr 's/^deployment_name\s*=\s*"([^"]*)".*$/\1/p'           "$DATAFILE")
-CREDENTIALS=$(sed -nr 's/^credentials\s*=\s*"([^"]*)".*$/\1/p'              "$DATAFILE")
-
-if [ ! -f "$DATAFILE" ]; then
-  echo "setenv: Configuration file not found: $DATAFILE"
-  return 1
-fi
-
-if [ -z "$PROJECT" ]
+elif  [ "$(curl -s -H "Authorization: token $GIT_TOKEN" 'https://api.github.com' -I | awk '{print $2}' | head -1)" == "200"  ]
 then
-    echo "Inside <$DATAFILE> the <google_project_id> not found trying to find from <common_configuration.tfvars>"
-    PROJECT=$(sed -nr 's/^google_project_id\s*=\s*"([^"]*)".*$/\1/p'  "$PWD/common_configuration.tfvars")
-    echo "Using FuchiCorp Google Project ID for deployment. <google_project_id> : <$PROJECT>"
+    wget --quiet "--header=Authorization: token $GIT_TOKEN" "https://raw.githubusercontent.com/fuchicorp/common_scripts/master/set-environments/terraform/google-set-env.sh" \
+  -O set-env >/dev/null
+source 'set-env' "$@"
+rm -rf "set-env"
 fi
-
-if [ -z "$BUCKET" ]
-then
-  echo "Inside <$DATAFILE> the <google_bucket_name> not found trying to find from <common_configuration.tfvars>"
-  BUCKET=$(sed -nr 's/^google_bucket_name\s*=\s*"([^"]*)".*$/\1/p'   "$PWD/common_configuration.tfvars")
-  echo "Using FuchiCorp Google Bucket name for deployment. <google_bucket_name>: <$BUCKET>"
-fi
-
-if [ -z "$ENVIRONMENT" ]
-then
-    echo "setenv: 'deployment_environment' variable not set in configuration file."
-    return 1
-fi
-
-if [ -z "$BUCKET" ]
-then
-  echo "setenv: 'google_bucket_name' variable not set in configuration file."
-  return 1
-fi
-
-if [ -z "$PROJECT" ]
-then
-    echo "setenv: 'google_project_id' variable not set in configuration file."
-    return 1
-fi
-
-if [ -z "$CREDENTIALS" ]
-then
-    echo "setenv: 'credentials' file not set in configuration file."
-    return 1
-fi
-
-if [ -z "$DEPLOYMENT" ]
-then
-    echo "setenv: 'deployment_name' variable not set in configuration file."
-    return 1
-fi
-
-cat << EOF > "$PWD/backend.tf"
-terraform {
-  backend "gcs" {
-    bucket  = "${BUCKET}"
-    prefix  = "${ENVIRONMENT}/${DEPLOYMENT}"
-    project = "${PROJECT}"
-  }
-}
-EOF
-cat "$PWD/backend.tf"
-
-GOOGLE_APPLICATION_CREDENTIALS="${PWD}/${CREDENTIALS}"
-export GOOGLE_APPLICATION_CREDENTIALS
-export DATAFILE
-/bin/rm -rf "$PWD/.terraform" 2>/dev/null
-/bin/rm -rf "$PWD/common_configuration.tfvars" 2>/dev/null
-echo "setenv: Initializing terraform"
-terraform init #> /dev/null
